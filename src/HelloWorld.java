4cfcf15849b4f407293c7619f571e3a8158b46cf
A.Kosorotikov <darkringfire@gmail.com>
2022-07-07 16:58:54 +0800

    Add HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * A program that prints <code>Hello World!</code>
+ * 
+ * @author a.kosorotikov
+ * @version 1.0
+ *
+ */
+public class HelloWorld {
+	/**
+	 * Entry point
+	 * 
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
