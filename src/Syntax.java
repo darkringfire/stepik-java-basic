694f59908117b2d0a3c463dade8597e4db558f24
A.Kosorotikov <darkringfire@gmail.com>
2022-07-08 14:28:38 +0800

    Add isPowerOfTwo solution

--- src/Syntax.java
+++ src/Syntax.java
@@ -32,4 +32,14 @@ public class Syntax {
 	public static char charExpression(int a) {
 	    return (char)('\\' + a);
 	}
+	
+	/**
+	 * Checks if given <code>value</code> is a power of two.
+	 *
+	 * @param value any number
+	 * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+	 */
+	public static boolean isPowerOfTwo(int value) {
+	    return Integer.bitCount(Math.abs(value)) == 1;
+	}
 }
