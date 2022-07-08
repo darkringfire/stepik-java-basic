ec96815959d853d65733ac62022efe4589a28dad
A.Kosorotikov <darkringfire@gmail.com>
2022-07-08 11:41:12 +0800

    Add leapYearCount task

--- src/Syntax.java
+++ src/Syntax.java
@@ -8,4 +8,8 @@ public class Syntax {
 	public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
 		return ((a ^ b) == (c ^ d)) && ((a ^ c) == (b ^ d)) && ((a ^ b) || (a ^ c));
 	}
+
+	public static int leapYearCount(int year) {
+		return year / 4 - year / 100 + year / 400;
+	}
 }
