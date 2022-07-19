e173fbae82cc5bb78bec41a44f4f2d1e1702cf87
A.Kosorotikov <darkringfire@gmail.com>
2022-07-19 13:48:53 +0800

    Integrator

---
+++ src/Integrator.java
@@ -0,0 +1,21 @@
+import java.util.function.DoubleUnaryOperator;
+
+public class Integrator {
+
+	public static void main(String[] args) {
+		System.out.println(integrate(x -> 1, 0, 10));
+
+	}
+
+	public static double integrate(DoubleUnaryOperator f, double a, double b) {
+		final double STEP = 1e-6;
+		double x = a;
+		double integral = 0;
+		while (x < b) {
+			integral += f.applyAsDouble(x) * STEP;
+			x += STEP;
+		}
+		return integral;
+	}
+
+}
