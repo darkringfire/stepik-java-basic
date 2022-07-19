3ee1dff1a5c1344299908466964f01ed1e110b90
A.Kosorotikov <darkringfire@gmail.com>
2022-07-19 13:43:54 +0800

    RolesSpeech

--- src/RolesSpeech.java
+++ src/RolesSpeech.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class RolesSpeech {
 
@@ -11,11 +13,28 @@ public class RolesSpeech {
 				"Лука Лукич: Господи боже! еще и с секретным предписаньем!"
 
 		};
+		System.out.println(">>>");
 		System.out.println(printTextPerRole(roles, textLines));
+		System.out.println(">>>");
 	}
 
 	private static String printTextPerRole(String[] roles, String[] textLines) {
-		String[] roleLines = new String[roles.length];
-		return "";
+		int rolesLength = roles.length;
+		Map<String, Integer> rolesOrder = new HashMap<>();
+		StringBuilder[] roleLines = new StringBuilder[rolesLength];
+		for (int i = 0; i < rolesLength; i++) {
+			roleLines[i] = new StringBuilder(roles[i] + ":\n");
+			rolesOrder.put(roles[i], i);
+		}
+
+		for (int i = 0; i < textLines.length; i++) {
+			String[] speach = textLines[i].split(": ", 2);
+			String roleName = speach[0];
+			int roleNumber = rolesOrder.get(roleName);
+			String speachLine = String.valueOf(i + 1) + ") " + speach[1];
+			roleLines[roleNumber].append(speachLine + "\n");
+		}
+
+		return String.join("\n", roleLines);
 	}
 }
